54e714fa0cb9d04c9d0fb8cd8a7ec48fcb8b5f0b
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-07-24 19:30:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# spustit kód, prozkoumat a odpovědět na otázky do komentáře
+# nezapomenout na Enter při vkládání textu do konzole
+
+name = "Billy " + "Black"
+
+print("Do you like programming, " + name + "?")
+like_programming = input()
+print("Great, " + name + ", you said " + like_programming + "!")
+
+more_programming = input("Do you want more programming, " + name + "? \n")
+print("Great, you said " + more_programming + " to more programming!")
+
+##### Úkol - Prozkoumej kód
+
+# Jakému řetězci bude přiřazena proměnná name na řádku 4?   
+ # Odpověď: 
+
+# Co by se stalo, pokud bych zapomněla na znak + na řádku 6?
+ # Odpověď: 
+
+# Co by se stalo, pokud bych výsledek funkce input  na řádku 7 neuložila do proměnné?
+ # Odpověď:
+
+# Co se stane s textem, který je vložen do závorek u input funkce na řádku 10 ?
+ # Odpověď:
